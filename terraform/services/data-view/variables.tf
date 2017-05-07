bf3a850c2b095cf3acc28158a238482be8c41045
John Spencer <johnspencer15@gmail.com>
2017-05-07 15:47:55 -0400

    data view packer template and terraform infra

---
+++ terraform/services/data-view/variables.tf
@@ -0,0 +1,31 @@
+variable "desired_capacity" {
+    default = "1"
+}
+
+variable "image_id" {
+  default =  "ami-f4c7a8e2"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "max_size" {
+    default = "1"
+}
+
+variable "min_size" {
+    default = "1"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "service_name" {
+    default = "data_view"
+}
+
+variable "vpc_id" {
+    default = "vpc-b4c6bdd2"
+}
